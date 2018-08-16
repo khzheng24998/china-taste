b074a1bde87acacd15b60d0589c66e07a55085eb
Kenny Zheng <Kenny.Zheng@mastercam.com>
2018-08-16 17:23:16 -0400

    Added "delete-item" and "update-item" functionality
    Added ability to delete/update items from order.

--- editOrder.js
+++ editOrder.js
@@ -22,6 +22,47 @@ struct OrderEntry
 }
 */
 
+function menuEntryIsEqual(a, b)
+{
+	let sameName = (a.name === b.name) ? 1 : 0;
+	let sameCategory = (a.category === b.category) ? 1 : 0;
+
+	let sameCost = 0;
+	if (a.cost.length === b.cost.length)
+	{
+		if (a.cost.length === 1)
+			sameCost = (a.cost[0] === b.cost[0]) ? 1 : 0;
+		else if (a.cost.length === 2)
+			sameCost = ((a.cost[0] === b.cost[0]) && (a.cost[1] === b.cost[1])) ? 1 : 0;
+	}
+
+	return (sameName && sameCategory && sameCost);
+}
+
+function orderEntryIsEqual(a, b)
+{
+	/*let sameMenuEntry = menuEntryIsEqual(a.menuEntry, b.menuEntry);
+	let sameSize = (a.size === b.size) ? 1 : 0;
+	let sameQuantity = (a.quantity === b.quantity) ? 1 : 0;
+	let sameSpecial = (a.special === b.special) ? 1 : 0;
+
+	return (sameMenuEntry && sameSize && sameQuantity && sameSpecial);*/
+
+	if (JSON.stringify(a) === JSON.stringify(b))
+		return true;
+
+	return false;
+}
+
+function orderLookup(orderEntry, order)
+{
+	for (let i = 0; i < order.length; i++)
+		if (orderEntryIsEqual(orderEntry, order[i]))
+			return i;
+
+	return -1;
+}
+
 //Return value: Returns true on success and false otherwise.
 //Argument(s):
 // - req: {action, name, size, quantity}
@@ -30,18 +71,15 @@ function updateOrder(req, order)
 {
 	switch(req.action)
 	{
-		case "add":
+		case "add-item":
 			addToOrder(req, order);
 			return true;
-		/*case "remove":
+		case "remove-item":
 			removeFromOrder(req, order);
 			return true;
-		case "change-quantity":
-			changeQuantity(req, order);
+		case "update-item":
+			updateItem(req, order);
 			return true;
-		case "change-size":
-			changeSize(req, order);
-			return true;*/
 		default:
 			console.log("updateOrder: Invalid action!");
 			return false;
@@ -99,14 +137,26 @@ function addToOrder(req, order)
 // - order: order list
 function removeFromOrder(req, order)
 {
-	/*let index = getOrderIndex(req.name, req.size, order);
+	let index = orderLookup(req.orderEntry, order);
 	if (index != -1)
 	{
 		order.splice(index, 1);
 		return true;
 	}
 
-	return false;*/
+	return false;
+}
+
+function updateItem(req, order)
+{
+	let index = orderLookup(req.orderEntry, order)
+	if (index != -1)
+	{
+		order[index] = req.newEntry;
+		return true;
+	}
+
+	return false;
 }
 
 //Return value: Returns true on success and false otherwise (note: a return value of false does not necessarily indicate program failure).
@@ -155,16 +205,6 @@ function changeSize(req, order)
 	}
 
 	return false;*/
-}
-
-//TODO: Remove this function
-function orderLookup(name, size, order)
-{
-	for (let i = 0; i < order.length; i++)
-		if (name == order[i].name && size == order[i].size)
-			return i;
-
-	return -1;
 }
 
 module.exports.updateOrder = updateOrder;
