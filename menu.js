85153e4af9d1ed4ef6e3436fb3fed0eb535f851a
khzheng24998 <kennyzheng249987@yahoo.com>
2018-08-15 23:46:51 -0400

    Reworked menu item selection feature
    Daily upload

--- menu.js
+++ menu.js
@@ -6,7 +6,10 @@ menu.categories = ["apps", "soup"];
 
 menu.apps = [];
 for (let i = 0; i < nApps; i++)
+{
 	menu.apps[i] = {};
+	menu.apps[i].category = "apps";
+}
 
 menu.apps[0].name = "1. Fried Whole Chicken Wings (5)";
 menu.apps[0].cost = [6.75];
@@ -20,7 +23,7 @@ menu.apps[2].cost = [1.65];
 menu.apps[3].name = "3. Spring Roll (1) (Only Veg.)";
 menu.apps[3].cost = [1.75];
 
-menu.apps[4].name = "3a. Spring Roll (1) (Shrimp & Veg.)";
+menu.apps[4].name = "3a. Spring Roll (1) (Shrimp \& Veg.)";
 menu.apps[4].cost = [1.55];
 
 menu.apps[5].name = "4. Bar-B-Q Spare Ribs (4)";
@@ -31,7 +34,10 @@ menu.apps[6].cost = [7.95, 11.50];
 
 menu.soup = [];
 for(let i = 0; i < nSoups; i++)
+{
 	menu.soup[i] = {};
+	menu.soup[i].categories = "soup";
+}
 
 menu.soup[0].name = "12. Wonton Soup";
 menu.soup[0].cost = [2.85, 4.35];
