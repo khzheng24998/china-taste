24cac57ea43aa4dba941c7657e91134a6cfa9ffa
khzheng24998 <kennyzheng249987@yahoo.com>
2018-09-05 00:10:08 -0400

    Updated server-side file hierarchy - part II
    Still need to go into code and change areas where old files were
    referenced

---
+++ Frontend/js/functions.js
@@ -0,0 +1,20 @@
+function isUserSignedIn()
+{
+	$.get("/account-status", function(data, status)
+	{
+		if (status !== "success")
+    		alert("An issue occurred signing out from system!\nIf this problem persists, please call us at (860) 871-9311.");
+		else
+		{
+			let label = (data.msg === "signed-in") ? "My Account &nbsp;&#9660;" : "Login";
+			$("#account-login-opt").html(label);
+		}
+	});
+}
+
+function resizePage()
+{
+	let winHeight = window.innerHeight;
+	winHeight = Math.floor(0.8 * winHeight);
+	$("#page-body").css("min-height", winHeight);
+}
